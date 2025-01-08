fe8bfe1a3aea68651c07fdbb58106b95fe6de613
Michelle Jin <mjin1812@gmail.com>
2025-01-08 17:37:44 -0500

    consolidated simplify by keywords tests to one doc

--- tests/testthat/test-simplify_by_keywords.R
+++ tests/testthat/test-simplify_by_keywords.R
@@ -22,3 +22,23 @@ test_that("Test that simplifying keywords works for all ontologys", {
                                     ontology = "unified"),
                df2)
 })
+
+
+
+
+test_that("Test that simplifying vector of acronyms by keywords works for all ontologys", {
+  # Test Allen Atlas
+  df <- dplyr::tibble(name = c("Dorsal part of the lateral geniculate complex",
+                               "Gustatory areas", "Field CA1 (dorsal)"), acronym = c("LGd", "GU", "dCA1" ))
+  acronyms <- c("LGd", "GU4", "dCA1so" )
+  expect_equal(simplify_vec_by_keywords(acronyms), df)
+
+  # Test Kim Unified Atlas
+  df <- dplyr::tibble(name = c("Lateral parabrachial nucleus",
+                               "Medial parabrachial nucleus",
+                               "Caudate Putamen"), acronym = c("LPB", "MPB", "CP"))
+  acronyms <- c("LPBD", "MPBE", "CPre")
+  expect_equal(simplify_vec_by_keywords(acronyms,
+                                        keywords = c("dorsal part", "external part", "Caudoputamen-"),
+                                        ontology = "unified"), df)
+})
