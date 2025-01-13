bb808dca4f85f479693422fbf0dd3285bbeb3668
Michelle Jin <mjin1812@gmail.com>
2025-01-13 14:00:59 -0500

    added unit tests for get_correlations

--- tests/testthat/test-get_correlations.R
+++ tests/testthat/test-get_correlations.R
@@ -1,17 +1,32 @@
-test_that("getting correlations works", {
 
+test_that("check that the values vector should match the by vector in get_correlations", {
   skip_on_cran()
   skip_on_ci()
 
-  path <- file.path(test_object_path("experiment"), "experiment.RDS")
-  e <- readRDS(file = path)
+  e <- make_test_experiment_light()
+  e$correlation_list <- NULL
 
-  expect_error(e <- get_correlations(e,
-                       by = c("group"),
-                       values = c("Context"),
-                       channels = c("eyfp"),
-                       p_adjust_method = "none",
-                       alpha = 0.01),
-               regexp = NA)
+  expect_warning(get_correlations(e, by = "group", values = c("Context", "Shock"),
+                                  channels = "cfos", , method = "pearson"),
+                 regexp = "Your values vector")
+})
+
+
+
+test_that("spearman and pearson options work for get_correlation",{
+  skip_on_cran()
+  skip_on_ci()
+  e <- make_test_experiment_light()
+  e$correlation_list <- NULL
+
+  e <- get_correlations(e, by = "group", values = c("Context"),
+                        channels = "cfos", method = "spearman")
+
+  expect_snapshot(e$correlation_list$Context$cfos$r)
+
+  e$correlation_list <- NULL
+  e <- get_correlations(e, by = "group", values = c("Context", "Shock"),
+                        channels = "cfos", , method = "pearson")
+  expect_snapshot(e$correlation_list$Context$cfos$r)
 
 })
